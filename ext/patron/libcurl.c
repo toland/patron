db952d86705f12f1bd88b84d77626254adcd8fa2
Phillip Toland <ptoland@thehive.com>
2009-01-07 16:36:36 -0600

    Added support for more callbacks.

--- ext/patron/libcurl.c
+++ ext/patron/libcurl.c
@@ -8,13 +8,29 @@ struct curl_state {
   CURL* handle;
 };
 
-void Init_libcurl();
 
-static size_t libcurl_block_shim(char* stream, size_t size, size_t nmemb, VALUE proc) {
+//------------------------------------------------------------------------------
+// Callback support
+//
+
+static size_t libcurl_write_shim(char* stream, size_t size, size_t nmemb, VALUE proc) {
   size_t result = size * nmemb;
   rb_funcall(proc, rb_intern("call"), 1, rb_str_new(stream, result));
   return result;
 }
+
+static size_t libcurl_read_shim(char* stream, size_t size, size_t nmemb, VALUE proc) {
+  size_t result = size * nmemb;
+  VALUE string = rb_funcall(proc, rb_intern("call"), 1, result);
+  size_t len = RSTRING(string)->len;
+  memcpy(stream, RSTRING(string)->ptr, len);
+  return len;
+}
+
+
+//------------------------------------------------------------------------------
+// Object allocation
+//
 
 void libcurl_free(struct curl_state *curl) {
   curl_easy_cleanup(curl->handle);
@@ -26,6 +42,11 @@ VALUE libcurl_alloc(VALUE klass) {
   VALUE obj = Data_Make_Struct(klass, struct curl_state, NULL, libcurl_free, curl);
   return obj;
 }
+
+
+//------------------------------------------------------------------------------
+// Method implementations
+//
 
 VALUE libcurl_version(VALUE klass) {
   char* value = curl_version();
@@ -78,13 +99,34 @@ VALUE libcurl_setopt(VALUE self, VALUE optval, VALUE parameter) {
 
   int option = FIX2INT(optval);
   switch (option) {
+    case CURLOPT_READFUNCTION:
+      curl_easy_setopt(curl, CURLOPT_READFUNCTION, &libcurl_read_shim);
+      curl_easy_setopt(curl, CURLOPT_READDATA, parameter);
+      break;
+
     case CURLOPT_WRITEFUNCTION:
-      curl_easy_setopt(curl->handle, CURLOPT_WRITEFUNCTION, &libcurl_block_shim);
-      curl_easy_setopt(curl->handle, CURLOPT_WRITEDATA, parameter);
+      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &libcurl_write_shim);
+      curl_easy_setopt(curl, CURLOPT_WRITEDATA, parameter);
+      break;
+
+    case CURLOPT_HEADERFUNCTION:
+      curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, &libcurl_write_shim);
+      curl_easy_setopt(curl, CURLOPT_HEADERDATA, parameter);
       break;
 
     default:
-      curl_easy_setopt(curl->handle, option, RSTRING(parameter)->ptr);
+      switch (TYPE(parameter)) {
+        case T_STRING:
+          curl_easy_setopt(curl->handle, option, RSTRING(parameter)->ptr);
+          break;
+
+        case T_FIXNUM:
+          curl_easy_setopt(curl->handle, option, FIX2INT(parameter));
+          break;
+
+        default:
+          // TODO raise an exception or something
+      }
   }
 
   return Qnil;
@@ -109,6 +151,11 @@ VALUE libcurl_perform(VALUE self) {
   return Qnil;
 }
 
+
+//------------------------------------------------------------------------------
+// Extension initialization
+//
+
 void Init_libcurl() {
   curl_global_init(CURL_GLOBAL_NOTHING);
 
@@ -117,7 +164,10 @@ void Init_libcurl() {
 
   rb_define_const(cLibcurl, "OPT_URL", INT2FIX(CURLOPT_URL));
   rb_define_const(cLibcurl, "OPT_HTTPGET", INT2FIX(CURLOPT_HTTPGET));
+
+  rb_define_const(cLibcurl, "OPT_READ_HANDLER", INT2FIX(CURLOPT_READFUNCTION));
   rb_define_const(cLibcurl, "OPT_WRITE_HANDLER", INT2FIX(CURLOPT_WRITEFUNCTION));
+  rb_define_const(cLibcurl, "OPT_HEADER_HANDLER", INT2FIX(CURLOPT_HEADERFUNCTION));
 
   rb_define_const(cLibcurl, "INFO_URL", INT2FIX(CURLINFO_EFFECTIVE_URL));
 
