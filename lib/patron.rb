3811be5ec84496354cfdcb3ddede621c18d4dea5
Phillip Toland <ptoland@thehive.com>
2009-01-12 12:29:10 -0600

    Added Patron.version method.

--- lib/patron.rb
+++ lib/patron.rb
@@ -5,3 +5,10 @@ $:.unshift(cwd) unless $:.include?(cwd) || $:.include?(cwd.expand_path)
 
 require 'patron/version'
 require 'patron/session'
+
+module Patron #:nodoc:
+  # Returns the version number of the Patron library as a string
+  def self.version
+    Patron::VERSION::STRING
+  end
+end
