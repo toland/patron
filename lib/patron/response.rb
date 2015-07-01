cdd1dd17ed736dbcef0938ea0fe02c4579ce5abb
Leandro López (inkel) <inkel.ar@gmail.com>
2015-07-01 16:01:39 -0300

    Response status line is always the first line of header_data

--- lib/patron/response.rb
+++ lib/patron/response.rb
@@ -85,19 +85,19 @@ module Patron
     def parse_headers(header_data)
       @headers = {}
 
-      header_data.split("\r\n").each do |header|
-        if header =~ %r|^HTTP/1.[01]|
-          @status_line = header.strip
-        else
-          parts = header.split(':', 2)
-          unless parts.empty?
-            parts[1].strip! unless parts[1].nil?
-            if @headers.has_key?(parts[0])
-              @headers[parts[0]] = [@headers[parts[0]]] unless @headers[parts[0]].kind_of? Array
-              @headers[parts[0]] << parts[1]
-            else
-              @headers[parts[0]] = parts[1]
-            end
+      lines = header_data.split("\r\n")
+
+      @status_line = lines.shift
+
+      lines.each do |header|
+        parts = header.split(':', 2)
+        unless parts.empty?
+          parts[1].strip! unless parts[1].nil?
+          if @headers.has_key?(parts[0])
+            @headers[parts[0]] = [@headers[parts[0]]] unless @headers[parts[0]].kind_of? Array
+            @headers[parts[0]] << parts[1]
+          else
+            @headers[parts[0]] = parts[1]
           end
         end
       end
