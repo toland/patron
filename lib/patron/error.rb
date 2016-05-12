55d01821d24beafebda1e4cf55296729ace6f33c
Julik Tarkhanov <me@julik.nl>
2016-05-12 22:00:33 +0200

    Rework charset coercion
    Make charset coercion explicit and on-demand. Add `Response#decoded_body` that
    performs best-effort charset coercion or fails early (and loudly) if coercion is
    impossible. Make `Response#body` always return binary strings. Since encoding
    errors can be very confusing, make an attempt to provide extensive diagnostic
    information about what failed, where and for what reason.
    
    Only allow response headers to be ISO-8859-1 or UTF-8. Response header encoding
    is strictly specified by HTTP standards, but UTF-8 is also used sometimes
    (for things like Content-Disposition).

--- lib/patron/error.rb
+++ lib/patron/error.rb
@@ -31,29 +31,38 @@ module Patron
 
   # Gets raised when the URL passed to Patron used a protocol that it does not support.
   # This most likely the result of a misspelled protocol string.
-  class UnsupportedProtocol   < Error; end
+  class UnsupportedProtocol    < Error; end
 
   # Gets raised when a request is attempted with an unsupported SSL version.
-  class UnsupportedSSLVersion < Error; end
+  class UnsupportedSSLVersion  < Error; end
 
   # Gets raised when the URL was not properly formatted.
-  class URLFormatError        < Error; end
+  class URLFormatError         < Error; end
 
   # Gets raised when the remote host name could not be resolved.
-  class HostResolutionError   < Error; end
+  class HostResolutionError    < Error; end
 
   # Gets raised when failing to connect to the remote host.
-  class ConnectionFailed      < Error; end
+  class ConnectionFailed       < Error; end
 
   # Gets raised when the response was shorter or larger than expected.
   # This happens when the server first reports an expected transfer size,
   # and then delivers data that doesn't match the previously given size.
-  class PartialFileError      < Error; end
+  class PartialFileError       < Error; end
 
   # Gets raised on an operation timeout. The specified time-out period was reached.
-  class TimeoutError          < Error; end
+  class TimeoutError           < Error; end
 
   # Gets raised on too many redirects. When following redirects, Patron hit the maximum amount.
-  class TooManyRedirects      < Error; end
+  class TooManyRedirects       < Error; end
 
+  # Gets raised when the server specifies an encoding that could not be found, or has an invalid name,
+  # or when the server "lies" about the encoding of the response body (such as can be the case
+  # when the server specifies an encoding in `Content-Type`) which the HTML generator then overrides
+  # with a `meta` element.
+  class HeaderCharsetInvalid < Error; end
+  
+  # Gets raised when you try to use `decoded_body` but it can't
+  # be represented by your Ruby process's current internal encoding
+  class NonRepresentableBody < HeaderCharsetInvalid; end
 end
