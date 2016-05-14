be4907d6699907f0bb5026a2f5e919475a000850
Julik Tarkhanov <me@julik.nl>
2016-05-15 00:25:11 +0200

    Update the failure message
    when no libraries can be found

--- ext/patron/extconf.rb
+++ ext/patron/extconf.rb
@@ -35,7 +35,7 @@ elsif !have_library('curl') or !have_header('curl/curl.h')
   fail <<-EOM
   Can't find libcurl or curl/curl.h
 
-  Try passing --with-curl-dir or --with-curl-lib and --with-curl-include
+  Try passing --with-curl-config, --with-curl-dir, or --with-curl-lib and --with-curl-include
   options to extconf.
   EOM
 end
