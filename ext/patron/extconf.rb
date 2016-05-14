80b0d51267918cb67edbe65555ac904253e2f522
Julik Tarkhanov <me@julik.nl>
2016-05-15 00:23:58 +0200

    Simplify curl-config flags templating

--- ext/patron/extconf.rb
+++ ext/patron/extconf.rb
@@ -27,13 +27,10 @@ require 'mkmf'
 require 'rbconfig'
 
 dir_config('curl')
-
-if curl_config_path = with_config('curl-config')
-  $CFLAGS << " #{`#{curl_config_path} --cflags`.strip}"
-  $LIBS << " #{`#{curl_config_path} --libs`.strip}"
-elsif find_executable('curl-config')
-  $CFLAGS << " #{`curl-config --cflags`.strip}"
-  $LIBS << " #{`curl-config --libs`.strip}"
+curl_config_path = with_config('curl-config') || find_executable('curl-config')
+if curl_config_path
+  $CFLAGS << " " << `#{curl_config_path} --cflags`.strip
+  $LIBS << " " << `#{curl_config_path} --libs`.strip
 elsif !have_library('curl') or !have_header('curl/curl.h')
   fail <<-EOM
   Can't find libcurl or curl/curl.h
