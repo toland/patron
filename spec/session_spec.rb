0280820c7ed89da51bf339055c0783dd2866d193
Julik Tarkhanov <me@julik.nl>
2017-11-07 18:02:09 +0100

    We need to add a trap _inside_ the thread

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -178,33 +178,44 @@ describe Patron::Session do
     expect {@session.get("/slow")}.to raise_error(Patron::TimeoutError)
   end
 
-  it "is able to terminate the thread that is running a slow request" do
+  it "is able to terminate the thread that is running a slow request using Thread#kill (uses the custom unblock)" do
     t = Thread.new do
       session = Patron::Session.new
       session.timeout = 40
       session.base_url = "http://localhost:9001"
       session.get("/slow")
     end
-    sleep 3
-    t.kill
+
+    # Our test server starts sending the body only after 20 seconds. We should be able to abort
+    # using a signal during that time.
+    started = Time.now.to_i
+    sleep 5 # Less than what it takes for the server to respond
+    t.kill # Kill the thread forcibly
+    t.join # wrap up the thread. If Patron is still busy there, this join call will still take 15s.
+
+    delta_s = Time.now.to_i - started
+    expect(delta_s).to be_within(2).of(5)
   end
 
   it "is able to terminate the thread that is running a slow request" do
     t = Thread.new do
+      trap('SIGINT') do
+        exit # exit the thread
+      end
       session = Patron::Session.new
       session.timeout = 40
       session.base_url = "http://localhost:9001"
       session.get("/slow")
     end
-    sleep 10
-    t.join
-  end
 
-  it "is able to terminate main thread that is running a slow request" do
-    session = Patron::Session.new
-    session.timeout = 40
-    session.base_url = "http://localhost:9001"
-    session.get("/slow")
+    # Our test server starts sending the body only after 20 seconds. We should be able to abort
+    # using a signal during that time.
+    started = Time.now.to_i
+    sleep 5 # Less than what it takes for the server to respond
+    Process.kill("INT", Process.pid) # Signal ourselves...
+    t.join # wrap up the thread. If Patron is still busy there, this join call will still take 15s.
+    delta_s = Time.now.to_i - started
+    expect(delta_s).to be_within(2).of(5)
   end
 
   it "should follow redirects by default" do
