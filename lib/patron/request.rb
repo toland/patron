4b7809821504ee7da62c012752ccc6b5810550c6
Julik Tarkhanov <me@julik.nl>
2018-01-18 13:09:13 +0100

    Add DNS cache timeout

--- lib/patron/request.rb
+++ lib/patron/request.rb
@@ -22,14 +22,14 @@ module Patron
 
     READER_VARS = [
       :url, :username, :password, :file_name, :proxy, :proxy_type, :insecure,
-      :ignore_content_length, :multipart, :action, :timeout, :connect_timeout,
+      :ignore_content_length, :multipart, :action, :timeout, :connect_timeout, :dns_cache_timeout,
       :max_redirects, :headers, :auth_type, :upload_data, :buffer_size, :cacert,
       :ssl_version, :http_version, :automatic_content_encoding, :force_ipv4, :download_byte_limit,
       :low_speed_time, :low_speed_limit, :progress_callback
     ]
 
     WRITER_VARS = [
-      :url, :username, :password, :file_name, :proxy, :proxy_type, :insecure,
+      :url, :username, :password, :file_name, :proxy, :proxy_type, :insecure, :dns_cache_timeout,
       :ignore_content_length, :multipart, :cacert, :ssl_version, :http_version, :automatic_content_encoding, :force_ipv4, :download_byte_limit,
       :low_speed_time, :low_speed_limit, :progress_callback
     ]
