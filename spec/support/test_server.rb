fe614f0972d7366fb8e709debc1a43c9fe17f7d2
Julik Tarkhanov <me@julik.nl>
2018-01-31 18:26:09 +0100

    No __dir__ on 1.9

--- spec/support/test_server.rb
+++ spec/support/test_server.rb
@@ -3,21 +3,18 @@ require 'puma'
 require 'rack/handler/puma'
 
 class PatronTestServer
-  APP = Rack::Builder.new { eval(File.read(__dir__ + '/config.ru')) }
+  APP = Rack::Builder.new { eval(File.read(File.dirname(__FILE__) + '/config.ru')) }
 
   def self.start(ssl = false, port = 9001 )
     @ssl = ssl
-    keypath = File.expand_path(__dir__ + '/../certs/privkey.pem')
-    certpath = File.expand_path(__dir__ + '/../certs/cacert.pem')
+    keypath = File.expand_path(File.read(File.dirname(__FILE__) + '/../certs/privkey.pem'))
+    certpath = File.expand_path(File.read(File.dirname(__FILE__) + '/../certs/cacert.pem'))
 
     host = if ssl
       'ssl://127.0.0.1:%d?key=%s&cert=%s' % [port, keypath, certpath]
     else
       'tcp://127.0.0.1:%d' % port
     end
-    $stderr.puts host.inspect
-    Rack::Handler::Puma.run(APP, {:Port => port.to_i, :Verbose => true, :Host => '0.0.0.0'}) {|server|
-      # @server = server
-    }
+    Rack::Handler::Puma.run(APP, {:Port => port.to_i, :Verbose => true, :Host => '0.0.0.0'})
   end
 end
