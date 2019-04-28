d587afc9ba763e3822cd5f3001cadc586a7fa4a5
Vladimir Kochnev <hashtable@yandex.ru>
2019-04-29 02:39:09 +0300

    Run Puma servers in separate processes.
    RSpec and Puma conflicts in SIGINT handling.
    When you first press Ctrl+C, RSpec sets `RSpec.world.wants_to_quit = true`
    to gracefully wait for currently running example to finish and then
    do the cleanup. Second Ctrl+C press force-terminates the process.
    
    This logic is overriden by Puma since `trap('INT')` is global to the
    process. The solution is to run Puma in separate process.

--- spec/support/test_server.rb
+++ spec/support/test_server.rb
@@ -6,6 +6,12 @@ class PatronTestServer
   APP = Rack::Builder.new { eval(File.read(File.join(__dir__, 'config.ru'))) }
 
   def self.start(ssl = false, port = 9001 )
+    # Reset the RSpec's SIGINT handler that does not really terminate after
+    # the first Ctrl+C pressed.
+    # Useful to terminate the forked process before Puma is actually started:
+    # it happens when running one particular example that does not need Puma
+    # so the specs are in fact finished before the Puma started.
+    Signal.trap('INT', 'EXIT')
     @ssl = ssl
     keypath = File.join(__dir__, 'certs', 'privkey.pem')
     certpath = File.join(__dir__, 'certs', 'cacert.pem')
